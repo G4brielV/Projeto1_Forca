6d6a597fb03c9738a82455c3fcf53c076006b1fe
Gabriel Vinícius <163596193+G4brielV@users.noreply.github.com>
2024-07-25 21:44:21 -0300

    Documentado

--- Jogo.py
+++ Jogo.py
@@ -1,50 +1,60 @@
-from kivy.app import App
-from unidecode import unidecode
-from Paises import paises
-from random import choice
+from kivy.app import App # Importar o app do kivy para o codigo
+from unidecode import unidecode # Tratar os acentos e ç
+from Paises import paises # "Banco de dados" com todos os nomes dos paises
+from random import choice # Randomizar a escolha dos paises
 
 class Forca():
     def __init__(self):
-        meu_aplicativo = App.get_running_app()
-        pagina_jogo = meu_aplicativo.root.ids["jogo"]
-        pagina_jogo.ids["img_forca"].source = "fotos/forca.png"
-        pagina_jogo.ids["letras_usadas"].text = ""
-        self.palavra = None
-        self.espaco_e_traco = None
-        self.vida = 0
-        self.letras_usadas = ""
-        self.letras_palavra = None
+        '''Zerando as telas para o inicio/reinicio'''
+        meu_aplicativo = App.get_running_app() # Puxando o App para conseguir edita-lo
+        pagina_jogo = meu_aplicativo.root.ids["jogo"] # Selecionando a tela pelo id
+        pagina_jogo.ids["img_forca"].source = "fotos/forca.png" # Mudando a imagem da página
+        pagina_jogo.ids["letras_usadas"].text = "" # Zerando a label
 
+        self.palavra = None # A palavra sorteada
+        self.vida = 0 # Quantidade de vida do jogador
+        self.letras_usadas = "" # Todas as letras ja testadas
+        self.letras_palavra = None # Todas as letras da palavra, sem repetições, acentos ou caractere especial
+
+    # Sortear o paísc
     def achar_pais(self, continente = " "):
+        # Caso ele escolha "Todos os países"
         if continente == " ":
             continente = choice(["Africa", "Americas", "Asia", "Europa", "Oceania"])
-        self.palavra = choice(paises[continente])
-        self.espaco_e_traco = [e for e,p in enumerate(self.palavra) if p in '- ']
+        self.palavra = choice(paises[continente]) # Sorteando o pais
         self.letras_palavra = set(unidecode(self.palavra.upper().replace(" ", "").replace("-", "")))
+        self.text_inicio()
+
+    # Criar o texto inicial da tela do jogo
+    def text_inicio(self):
+        # Criando o primeiro texto do jogo, com os devidos espaços e " - "
         inicio = ['_' if x not in '- ' else x for x in self.palavra.upper()]
-        for posi in self.espaco_e_traco:
-            inicio[posi] = self.palavra[posi]
-        str_inicio = " ".join(inicio)
+        str_inicio = " ".join(inicio) # tranformando em str, para o kivy aceitar
+
+        # Modificando o texto na tela
         meu_aplicativo = App.get_running_app()
         pagina_jogo = meu_aplicativo.root.ids["jogo"]
         pagina_jogo.ids["espaco_letras"].text = str_inicio
 
+    # Atualizar a palavra na tela do jogo com os acertos
     def adicionar_letras(self):
-        nivel_atual = [self.palavra.upper()[e] if letra in self.letras_usadas else '_' for e,letra in enumerate(unidecode(self.palavra.upper()))]
-        for posi in self.espaco_e_traco:
-            nivel_atual[posi] = self.palavra[posi]
+        nivel_atual = [self.palavra.upper()[e] if letra in self.letras_usadas or letra in '- ' else '_' for e,letra in enumerate(unidecode(self.palavra.upper()))]
         str_nivel_atual = " ".join(nivel_atual)
+
+        # Modificando o texto na tela
         meu_aplicativo = App.get_running_app()
         pagina_jogo = meu_aplicativo.root.ids["jogo"]
         pagina_jogo.ids["espaco_letras"].text = str_nivel_atual
 
-
+    # Funcionalidades padrões do jogo
     def game(self, chute):
         meu_aplicativo = App.get_running_app()
         pagina_jogo = meu_aplicativo.root.ids["jogo"]
         chute = unidecode(chute.upper())
+
         # Zerando o quadro de avisos
         pagina_jogo.ids["aviso"].text = ""
+
         # Verifcando se é uma letra válida
         if chute.isalpha() and len(chute) == 1:
             # Verificando se a letra ja não foi testada
@@ -52,20 +62,18 @@ class Forca():
                 pagina_jogo.ids["aviso"].text = f"Voçê ja digitou a letra: {chute}."
             else:
                 self.letras_usadas += f" {chute}"
-                # print(self.letras_usadas)
                 pagina_jogo.ids["letras_usadas"].text = self.letras_usadas
-                # Verificando se a letra ta na palavra
+                # Verificando se existe a letra chutada na palavra
                 if chute in self.letras_palavra:
                     self.letras_palavra.remove(chute)
                     self.adicionar_letras()
-                    # print(self.letras_palavra)
+                    # Verificando se todas as letras ja foram acertadas
                     if len(self.letras_palavra) == 0:
                         self.ganhou()
-                # Se a letra não estive na palavra
                 else:
                     # Perde uma vida
                     self.vida += 1
-                    # Mudando a imagem
+                    # Mudando a imagem de acordo com a vida
                     if self.vida == 1:
                         pagina_jogo.ids["img_forca"].source = "fotos/boneco_cabeca.png"
                     elif self.vida == 2:
@@ -77,14 +85,17 @@ class Forca():
                     elif self.vida == 5:
                         pagina_jogo.ids["img_forca"].source = "fotos/boneco_1_braco.png"
                     elif self.vida == 6:
-                        pagina_jogo.ids["img_forca"].source = "fotos/boneco_2_perna.png"
+                        pagina_jogo.ids["img_forca"].source = "fotos/boneco_2_braco.png"
+                    elif self.vida == 7:
                         self.perdeu()
         else:
             pagina_jogo.ids["aviso"].text = f'Digite uma letra, "{chute}" não é válido'
 
+    # Atualizar a tela final para o cenário de vencedor
     def ganhou(self):
         meu_aplicativo = App.get_running_app()
         pagina_final = meu_aplicativo.root.ids["final"]
+
         pagina_final.ids["img_final"].source = "fotos/vivo.png"
         pagina_final.ids["text_final"].color = 0, 1, 0
         pagina_final.ids["text_final"].text = "Parabéns"
@@ -92,9 +103,11 @@ class Forca():
         pagina_final.ids["btn_final"].text = "Recomeçar"
         meu_aplicativo.mudar_tela("final")
 
+    # Atualizar a tela final para o cenário de perdedor
     def perdeu(self):
         meu_aplicativo = App.get_running_app()
         pagina_final = meu_aplicativo.root.ids["final"]
+
         pagina_final.ids["img_final"].source = "fotos/morto.png"
         pagina_final.ids["text_final"].color = 1, 0, 0
         pagina_final.ids["text_final"].text = "Voçê Perdeu!!"
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from kivy.app import App
-from kivy.lang import Builder
+from kivy.app import App # Importando o app do kivy
+from kivy.lang import Builder # Imporando o builder para usar os .kv
 from kivy.uix.textinput import TextInput
-from telas import *
-from botoes import *
-from Jogo import Forca
+from telas import * # Importando as letras do App
+from botoes import * # Importando alguns botões diferentes
+from Jogo import Forca # Importando o código do jogo
 
-GUI = Builder.load_file("main.kv")
+GUI = Builder.load_file("main.kv") # Definindo o .kv do projeto
 class MainApp(App):
     def build(self):
         self.forca = Forca()
